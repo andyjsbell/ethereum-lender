6e4d3b3c5dac365ff438649f71f3fc6c889e607f
Andy Bell <andy.bell@displaynote.com>
2019-11-22 15:55:40 +0100

    Up and running with web3

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
+import getWeb3 from "./utils/getWeb3";
 
-function App() {
+const Home = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    Home
+    </>
+  )
+};
+
+class App extends Component {
+
+  state = {web3: null, accounts: []};
+  
+  handleChange(event, newValue) {
+    this.setState({value: newValue});
+  }
+
+  componentDidMount = async () => {
+    try {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.accounts;
+      const networkId = await web3.version.network;
+      // const deployedNetwork = ControllerContract.networks[networkId];
+      // const contract  = web3.eth.contract(ControllerContract.abi);
+      // const instance = contract.at(deployedNetwork.address);
+
+      this.setState({ web3:web3, accounts:accounts });
+      
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contracts...</div>;
+    }
+    
+    return (
+      <>
+        <Home {...this.state}/>
+      </>
+    );
+  }
 }
 
 export default App;
